05ff9517ca780b151b7d84a109e341fce6588d40
Jeanderson Gomes <jenandersondesousagomes@gmail.com>
2022-01-21 15:57:41 -0300

    finalizando estudo de comandos basicos de CRUD do MongoDB

--- comandos_basicos.py
+++ comandos_basicos.py
@@ -18,7 +18,9 @@ def main():
     # Usando o objeto cliente, nos podemos acessar as
     # bases de dados. Para isso usamos o
     # list_database_names().
+    '''
     print(client.list_database_names())
+    '''
 
     # Voce pode criar uma base de dados usando o comando
     # a baixo.
@@ -34,7 +36,9 @@ def main():
 
     # ?? É possivel ver a lista de collections na base de dados
     # usando o seguinte comando ??
+    '''
     print(student_db.list_collection_names())
+    '''
 
     # Inserir documentos nas coleções
     # Podemos fazer isso de duas formas
@@ -52,12 +56,13 @@ def main():
     #Inserir varios
     '''
     documents = [{'Name':'Raj','Roll No': 153,
-                'Branch': 'CSE'},{'Name':'Raj',
-                'Roll No': 153,'Branch': 'CSE'},
-                 {'Name':'Raj','Roll No': 153,
-                'Branch': 'CSE'}]
+                'Branch': 'CSE'},{'Name':'Jeands',
+                'Roll No': 50,'Branch': 'BRA'},
+                 {'Name':'Folote','Roll No': 70,
+                'Branch': 'EUA'}]
     collection.insert_many(documents)
     '''
+
     # Um usuario tambem pode dar um id_customizado ao
     # inserir o documento.
 
@@ -71,10 +76,100 @@ def main():
     # No primeiro caso usamo o comando find_one(), e
     # fornecemos parametros paraconsultar do documento
     # esperado.
+    '''
     query = {"Name":"Raj"}
     print(collection.find_one(query))
+    '''
 
-    # No seguindo caso usamos o comando find_many()
+    # No seguindo caso usamos o comando find_many(), o
+    # retorno dessa função é um objeto cursor, sendo assim
+    # podemos usar um loop para acessar as informações:
+    '''
+    query = {"Branch":"CSE"}
+    result = collection.find(query)
+    for i in result:
+        print(i)
+    '''
+
+    # VOce pode itilisar como filtro os operadores gt,
+    # lt, eq e etc.
+
+    #"$eq" = Valor iguala ao especificado
+    '''
+    query = {"Roll No":{"$eq":153}}
+    print(collection.find_one(query))
+    '''
+
+    #"$gt" =  Maior que o valor especificado
+    '''
+    query = {"Roll No":{"$gt":50}}
+    print(collection.find_one(query))
+    '''
+
+    #"$gte" =  Maior ou igual ao valor especificado
+    '''
+    query = {"Roll No":{"$gte":153}}
+    print(collection.find_one(query))
+    '''
+
+    #"$lt" =  Menor que o valor especificado
+    '''
+    query = {"Roll No": {"$lt":70}}
+    print(collection.find_one(query))
+    '''
+
+    #"$lte" =  Menor ou igual ao valor especificado
+    '''
+    query = {"Roll No": {"$lte":50}}
+    #print(collection.find_one(query))
+    '''
+
+    #Atualizar o documento dentro da coleção
+    # Podemos atualizar de duas foras
+    # 1. Atualizando um unico documento
+    # 2. Atualizando varios documentos
+
+    # Um documento:
+    # Para atualizar precisamos de 2 parametros, o
+    # o primeiro parametro é o resultado da consulta
+    # realizada do documento que deseja atualizar e o
+    # segundo é o parametro de atualização.
+    '''
+    query = {"Roll No":{"$eq":153}}
+    present_data = collection.find_one(query)
+    new_data = {'$set':{"Name":'Jeronimo'}}
+    collection.update_one(present_data,new_data)
+    '''
+
+    # Muitos documentos
+    '''
+    present_data = {"Branch":"CSE"}
+    new_data = {"$set":{"Branch":'ECE'}}
+    collection.update_many(present_data,new_data)
+    '''
+
+    # Remover um documento de uma coleção
+    # Podemos remover um document de duas formas
+    # 1. Removendo 1 documento
+    # 2. Atualizando varios documentos
+
+    #Remover um:
+    '''
+    query = {"Roll No": 153}
+    collection.delete_one(query)
+    '''
+
+    # Revomer varios:
+    '''
+    query = {"Branch":"CSE"}
+    collection.delete_many(query)
+    '''
+
+    # Removendo a coleção de dados
+    # Basta apenas usar o comando .drop()
+    '''
+    collection.drop()
+    '''
 
 if __name__ == "__main__":
     main()
